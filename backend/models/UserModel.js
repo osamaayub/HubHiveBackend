9cc08b82559cf542c629ab3e0107c63975d8a6b8
Osama Ayub <ayubosama416@gmail.com>
2024-10-29 14:39:27 +0500

    Created backend for Job Portal

---
+++ backend/models/UserModel.js
@@ -0,0 +1,76 @@
+const mongoose = require('mongoose')
+
+
+const UserSchema = new mongoose.Schema({
+
+
+    name: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    password: {
+        type: String,
+        required: true,
+        min: 1,
+        max: 8
+    },
+
+    avatar: {
+        public_id: {
+            type: String,
+
+        },
+        url: {
+            type: String,
+
+        },
+    },
+
+    role: {
+        type: String,
+        enum: ["applicant", "admin"],
+        default: "applicant"
+    },
+
+    skills: [
+        {
+            type: String
+        }
+    ],
+
+    resume: {
+        public_id: {
+            type: String,
+            required: false
+        },
+        url: {
+            type: String,
+            required: false
+        },
+
+    },
+    savedJobs: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Job'
+        }
+    ],
+    appliedJobs: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Application'
+        }
+    ],
+
+
+}, { timestamp: true })
+
+const User = mongoose.model('User', UserSchema)
+module.exports = User
